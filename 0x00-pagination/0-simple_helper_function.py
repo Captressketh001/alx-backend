c4c6fb7167889818bea50592a81819464f35da4e
Captressketh001 <shininglite2012@gmail.com>
2023-07-25 07:04:05 +0000

    fixed pycodestyle

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """A simple helper function"""
 
+
 def index_range(page: int, page_size: int) -> tuple:
     """a function named index_range that takes two integer
     arguments page and page_size"""
