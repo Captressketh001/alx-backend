1c6a3976a111e2b1d7bf74b8e3ef72b66ed92956
Captressketh001 <shininglite2012@gmail.com>
2023-07-25 09:45:24 +0000

    fixed pycodestyle issue

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -28,12 +28,12 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
-      
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Get page"""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
-        
+
         start, end = index_range(page, page_size)
         data = self.dataset()
 
