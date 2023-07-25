5f4c6b8f2908ac0ba9aba3338428acd954e00885
Captressketh001 <shininglite2012@gmail.com>
2023-07-25 09:21:20 +0000

    pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -28,7 +28,7 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Get page"""
         assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) page_size > 0
+        assert isinstance(page_size, int) and page_size > 0
         
         start, end = index_range(page, page_size)
         data = self.dataset()
@@ -37,5 +37,4 @@ class Server:
 
         if start >= len(data):
             return result
-
-    return data[start:end]
+        return data[start:end]
