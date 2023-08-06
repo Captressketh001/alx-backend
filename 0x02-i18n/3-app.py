da68ea53b69e406f7760d8f7817bc050fa356873
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-08-06 01:15:12 +0100

    locale

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -32,9 +32,7 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def index():
     """Route 2.index.html"""
-    home_title = gettext("Welcome to Holberton")
-    home_header = gettext("Hello world")
-    return render_template("3-index.html", home_title=home_title, home_header=home_header)
+    return render_template("3-index.html")
 
 
 if __name__ == "__main__":
